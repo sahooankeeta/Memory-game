1193c6a3112154f435d97553598dd60c21744ec0
Ankeeta Sahoo <sahooankeeta@gmail.com>
2023-10-06 11:10:31 +0530

    socket

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import openSocket from 'socket.io-client';
 const URL = process.env.REACT_APP_ENV=== 'production' ? undefined :process.env.REACT_APP_API
-const socket = openSocket(URL, { transports : ['websocket'] });
+const socket = openSocket.connect(process.env.REACT_APP_API, { transports : ['websocket'] });
 
 const App=()=>{
   const user=useSelector(state=>state.authData)
